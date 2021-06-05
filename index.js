c7c27a4afd2e14686a9c472a8a0d508d1ec662d0
Titus Roby K <titusrobyk@gmail.com>
2021-06-05 16:39:03 +0530

    Index js is functional now...

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-require.config({ paths: { vs: "node_modules/monaco-editor/min/vs" } });
+"use strict";
+var editor = null;
 
+require.config({ paths: { vs: "node_modules/monaco-editor/min/vs" } });
 require(["vs/editor/editor.main"], function () {
-  var editor = monaco.editor.create(document.getElementById("editor"), {
+  editor = monaco.editor.create(document.getElementById("editor"), {
     value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
     language: "javascript",
   });
@@ -11,7 +13,6 @@ require(["vs/editor/editor.main"], function () {
       return lang.id;
     });
     modesIds.sort();
-
     return modesIds.map(function (modeId) {
       return {
         modeId: modeId,
@@ -28,4 +29,43 @@ require(["vs/editor/editor.main"], function () {
     }
     document.querySelector(".language-picker").append(o);
   }
+  document.querySelector(".language-picker").selectedIndex = startModeIndex;
+  loadSample(MODES[startModeIndex]);
+
+  document
+    .querySelector(".language-picker")
+    .addEventListener("change", function (evt) {
+      loadSample(MODES[this.selectedIndex]);
+    });
+
+  document
+    .querySelector(".theme-picker")
+    .addEventListener("change", function (evt) {
+      changeTheme(this.selectedIndex);
+    });
 });
+
+window.onresize = function () {
+  if (editor) {
+    editor.layout();
+  }
+};
+
+function loadSample(mode) {
+  if (!editor) {
+    editor = monaco.editor.create(document.getElementById("editor"), {
+      model: null,
+    });
+  }
+  var oldModel = editor.getModel();
+  var newModel = monaco.editor.createModel("", mode.modeId);
+  editor.setModel(newModel);
+  if (oldModel) {
+    oldModel.dispose();
+  }
+}
+
+function changeTheme(theme) {
+  var newTheme = theme === 1 ? "vs-dark" : theme === 0 ? "vs" : "hc-black";
+  monaco.editor.setTheme(newTheme);
+}
